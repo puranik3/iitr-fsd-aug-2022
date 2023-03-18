196d85dda2115db2b7839d572e396da258273489
Prashanth Puranik <puranik@digdeeper.in>
2023-03-18 13:49:04 +0530

    week 6 starter files

---
+++ demos/week-06/topics/05-prototype.js
@@ -0,0 +1,9 @@
+const john = {
+    name: 'John',
+    age: 32,
+};
+
+const jane = {
+    name: 'Jane',
+    age: 28,
+};
---
+++ demos/week-06/topics/06-array-iterator-methods.js
@@ -0,0 +1,7 @@
+const persons = [
+    { name: 'John', age: 32, city: 'Bangalore' },
+    { name: 'Jane', age: 28, city: 'Bangalore' },
+    { name: 'Mark', age: 40, city: 'Hyderabad' },
+    { name: 'Mary', age: 44, city: 'Hyderabad' },
+    { name: 'David', age: 60, city: 'Delhi' }
+];
