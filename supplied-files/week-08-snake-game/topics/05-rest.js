df63dd185cf3899ee65e1b4d2b93556980b4b496
Prashanth Puranik <puranik@digdeeper.in>
2023-04-08 15:18:47 +0530

    supplied files for week 8

---
+++ supplied-files/week-08-snake-game/topics/05-rest.js
@@ -0,0 +1,20 @@
+const days = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri'
+];
+
+const john = {
+    name: 'John',
+    age: 32,
+    address: {
+        city: 'Bengaluru',
+        state: 'Karnataka'
+    },
+    emails: [
+        'john@gmail.com',
+        'john@example.com'
+    ]
+};
---
+++ supplied-files/week-08-snake-game/topics/06-spread.js
@@ -0,0 +1,15 @@
+// ... (overloaded with "rest")
+
+// copy / merge objects and arrays
+const nums1 = [ 1, 2, 3 ], nums2 = [ 4, 5, 6 ];
+
+const john = {
+    name: 'John',
+    age: 32
+};
+
+const johnEmployment = {
+    name: 'Jonathan',
+    companyName: 'Example Consulting',
+    role: 'Fullstack Developer'
+};
